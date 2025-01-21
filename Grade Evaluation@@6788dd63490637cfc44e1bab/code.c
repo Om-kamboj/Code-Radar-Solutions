1b4558ba58477b675eb9b605caabd7a28f8bca84
Om-kamboj <omkamboj09@gmail.com>
2025-01-21 20:36:09 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    char c;
+    scanf("%c",&c);
+    if(c=='A'){
+        printf("Excellent");
+    }
+    else if(c=='B'){
+        printf("Good");
+    }
+    else if(c=='C'){
+        printf("Average");
+    }
+    else if(c=='D'){
+        printf("Below Average");
+    }
+    else if(c=='F'){
+        printf("Fail");
+    }
+    else{
+        printf("Invalid");
+    }
+    return 0;
+}
